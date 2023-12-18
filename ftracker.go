0e23518dd3224ac3872f5bc807b5e468ddf4a98d
kbdbvgm <kbd.bvgm@gmail.com>
2023-12-18 18:30:30 +0300

    Fix error in formula in WalkingSpentCalories func

--- ftracker.go
+++ ftracker.go
@@ -7,11 +7,11 @@ import (
 
 // Основные константы, необходимые для расчетов.
 const (
-	lenStep = 0.65 // средняя длина шага.
-	mInKm   = 1000 // количество метров в километре.
-	minInH  = 60   // количество минут в часе.
-	//kmhInMsec = 0.278 // коэффициент для преобразования км/ч в м/с.
-	//cmInM     = 100   // количество сантиметров в метре.
+	lenStep   = 0.65  // средняя длина шага.
+	mInKm     = 1000  // количество метров в километре.
+	minInH    = 60    // количество минут в часе.
+	kmhInMsec = 0.278 // коэффициент для преобразования км/ч в м/с.
+	cmInM     = 100   // количество сантиметров в метре.
 )
 
 // distance возвращает дистанцию(в километрах), которую преодолел пользователь за время тренировки.
@@ -99,8 +99,8 @@ const (
 // weight float64 — вес пользователя.
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
-	speed := math.Pow(meanSpeed(action, duration), 2)
-	return (walkingCaloriesWeightMultiplier*weight + (speed/height)*walkingSpeedHeightMultiplier*weight) * duration * minInH
+	speed := math.Pow(meanSpeed(action, duration)*kmhInMsec, 2)
+	return (walkingCaloriesWeightMultiplier*weight + (speed/(height/cmInM))*walkingSpeedHeightMultiplier*weight) * duration * minInH
 
 }
 
